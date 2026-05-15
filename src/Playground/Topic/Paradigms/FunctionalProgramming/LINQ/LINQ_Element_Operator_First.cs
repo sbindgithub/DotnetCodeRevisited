f91f0ac4ec9d922dc667d51c95c56ec8f0806c6e
Sarada Ranjan Behera <saradabro@gmail.com>
2026-05-15 19:48:03 +0530

    update

--- src/Playground/Topic/Paradigms/FunctionalProgramming/LINQ/LINQ_Element_Operator_First.cs
+++ src/Playground/Topic/Paradigms/FunctionalProgramming/LINQ/LINQ_Element_Operator_First.cs
@@ -3,20 +3,18 @@ using System.Collections;
 
 namespace Playground.Topic.Paradigms.FunctionalProgramming.LINQ;
 
-public class LINQ_FirstOrDefault : ExampleBase
+public class LINQ_Element_Operator_FirstOrDefault : ExampleBase
 {
-    public LINQ_FirstOrDefault()
-        : base("LINQ_FirstOrDefault Example", TopicType.LINQ)
+    public LINQ_Element_Operator_FirstOrDefault()
+        : base("LINQ_Element_Operator_FirstOrDefault Example", TopicType.LINQ)
     {
     }
 
     public override void Run()
     {
-        var numbers = new List<int> { 5, 10, 15 };
+        var numbers = new List<int> { 5, 10, 15, 20 };
+        var firstNumber = numbers.First(x => x > 10);
 
-        var greaterThan100 = numbers.FirstOrDefault(x => x > 100);
-        Console.WriteLine($"greaterThan100 FirstOrDefault:", greaterThan100);
-
-        Console.WriteLine("If no element matches, FirstOrDefault returns the default value of the type.\r\n\r\nFor int, default is 0.");
+        Console.WriteLine($"firstNumber greater than 10:",  firstNumber);
     }
 }
--- src/Playground/Topic/Paradigms/FunctionalProgramming/LINQ/LINQ_Element_Operator_FirstOrDefault.cs
+++ src/Playground/Topic/Paradigms/FunctionalProgramming/LINQ/LINQ_Element_Operator_FirstOrDefault.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 namespace Playground.Topic.Paradigms.FunctionalProgramming.LINQ;
 
-public class LINQ_Element_Operator_FirstOrDefault : ExampleBase
+public class LINQ_FirstOrDefault : ExampleBase
 {
-    public LINQ_Element_Operator_FirstOrDefault()
-        : base("LINQ_Element_Operator_FirstOrDefault Example", TopicType.LINQ)
+    public LINQ_FirstOrDefault()
+        : base("LINQ_FirstOrDefault Example", TopicType.LINQ)
     {
     }
 
     public override void Run()
     {
-        var numbers = new List<int> { 5, 10, 15, 20 };
-        var firstNumber = numbers.First(x => x > 10);
+        var numbers = new List<int> { 5, 10, 15 };
 
-        Console.WriteLine($"firstNumber greater than 10:",  firstNumber);
+        var greaterThan100 = numbers.FirstOrDefault(x => x > 100);
+        Console.WriteLine($"greaterThan100 FirstOrDefault:", greaterThan100);
+
+        Console.WriteLine("If no element matches, FirstOrDefault returns the default value of the type.\r\n\r\nFor int, default is 0.");
     }
 }
